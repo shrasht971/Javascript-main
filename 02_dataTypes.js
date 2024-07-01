a293785530b763f7e0d226947c168a83fc5c3244
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-07-01 15:13:31 +0530

    add on data types in javascript

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //Treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+let name = "shrashti"
+let age = 23
+let isLoggedIn = false
+let state; //undeined
+let temp=null;//empty
+
+// number =>2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => uniqueness find(unique)
+
+
+//object
+
+console.log(typeof "shrashti");
+console.log(typeof age);
+console.log(typeof null); //null is a object
+console.log(typeof undefined) //undefined is a type of undefined
