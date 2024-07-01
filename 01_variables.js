a1983a0a59c648fe9d4420fd603b896160d5b44e
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2024-07-01 14:48:20 +0530

    first commit

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14752
+let accountEmail = "shrashtisahu5@gmail.com"
+var accountPassword = 'w24cfgf334'
+accountCity = "Jaipur"
+let accountState;
+// accountId=4534214  //not allowed
+accountEmail = "stfdwoi2@hgvd"
+var accountPassword="fuydwqhd"
+accountState ="Lucknow"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
